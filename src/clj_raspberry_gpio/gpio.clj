70b00ecdf00853e818549d28dd7eb27bc2dd0945
Matti Ahinko <matti.ahinko@gofore.com>
2017-02-22 22:37:33 +0200

    Add system paths

--- src/clj_raspberry_gpio/gpio.clj
+++ src/clj_raspberry_gpio/gpio.clj
@@ -3,6 +3,29 @@
 (def low 0)
 (def high 1)
 
+(def -base-path "/sys/class/gpio")
+(def -setup-path (format "%s/export" base-path))
+(def -cleanup-path (format "%s/unexport" base-path))
+
+(defn -pin-path
+  [pin]
+  (format
+    "%s/gpio%d"
+    base-path
+    pin))
+
+(defn -pin-setup-path
+  [pin]
+  (format
+    "%s/direction"
+    (-pin-path pin)))
+
+(defn -pin-state-path
+  [pin]
+  (format
+    "%s/value"
+    (-pin-path pin)))
+
 (defn setup
   "Setup pin"
   [pin mode])
