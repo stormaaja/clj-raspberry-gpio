20d5faa2e78fdc7b9e8f1fdb3fb181a0983223b6
Matti Ahinko <matti.ahinko@gofore.com>
2017-02-22 22:50:13 +0200

    Add initial project file

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-raspberry-gpio "0.1.0"
+  :description "Simple Clojure library for using Raspberry Pi GPIO."
+  :url "https://github.com/stormaaja/clj-raspberry-gpio"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
