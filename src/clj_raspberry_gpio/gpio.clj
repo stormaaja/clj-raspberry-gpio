93ad988987d8ef558ad442ef8139e3fbe0b35bf3
Matti Ahinko <matti.ahinko@gofore.com>
2017-02-25 09:50:12 +0200

    Fix syntax

--- src/clj_raspberry_gpio/gpio.clj
+++ src/clj_raspberry_gpio/gpio.clj
@@ -8,79 +8,79 @@
 (def in "in")
 (def out "out")
 
-(def -base-path (:gpio-path env))
-(def -setup-path (format "%s/export" -base-path))
-(def -cleanup-path (format "%s/unexport" -base-path))
+(def base-path (:gpio-path env))
+(def setup-path (format "%s/export" base-path))
+(def cleanup-path (format "%s/unexport" base-path))
 
-(defn -pin-path
+(defn- pin-path
   [pin]
   (format
     "%s/gpio%d"
-    -base-path
+    base-path
     pin))
 
-(defn -pin-setup-path
+(defn- pin-setup-path
   [pin]
   (format
     "%s/direction"
-    (-pin-path pin)))
+    (pin-path pin)))
 
-(defn -pin-state-path
+(defn- pin-state-path
   [pin]
   (format
     "%s/value"
-    (-pin-path pin)))
+    (pin-path pin)))
 
 (defn setup
   "Setup pin"
   [pin mode]
-  (spit -setup-path pin)
-  (spit (-pin-setup-path pin) mode))
+  (spit setup-path pin)
+  (spit (pin-setup-path pin) mode))
 
 (defn input
   "Read pin value"
   [pin]
-  (slurp (-pin-path pin)))
+  (slurp (pin-path pin)))
 
 (defn output
   "Set pin state"
   [pin state]
-  (spit (-pin-state-path pin) state))
+  (spit (pin-state-path pin) state))
 
 (defn cleanup
   "Cleanup pin"
   [pin]
-  (spit -cleanup-path pin))
+  (spit cleanup-path pin))
 
-(defn -match-pin?
+(defn- match-pin?
   [pin-dir]
   (not=
     (re-matches #"gpio(\d{1,2})" pin-dir)
     nil))
 
-(defn -get-name
+(defn- get-name
   [file]
   (.getName file))
 
-(defn -list-pin-dirs
+(defn- list-pin-dirs
   []
   (filter
-    -match-pin?
+    match-pin?
     (map
-      -get-name
-      (file-seq (clojure.java.io/file -base-path)))))
+      get-name
+      (file-seq (clojure.java.io/file base-path)))))
 
-(defn -parse-pin-number
+(defn- parse-pin-number
   [pin-dir]
   (str/replace pin-dir #"gpio" ""))
 
-(defn -list-pins
+(defn- list-pins
   []
   (map
-    -parse-pin-number
-    (-list-pin-dirs)))
+    parse-pin-number
+    (list-pin-dirs)))
 
 (defn cleanup-all
   "Cleanup all pins"
   []
-  (dorun (map cleanup (-list-pins))))
+  (dorun (map cleanup (list-pins))))
